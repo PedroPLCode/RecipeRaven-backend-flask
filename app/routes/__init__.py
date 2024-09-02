4a1c7ebdcf88ddc91f8b49a8a452b4e8b51f9491
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-09-02 19:57:45 +0200

    Revert "Add APScheduler"
    This reverts commit 13a36f3f8d045612193b13709ac302dcab067bb6.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,14 +1,5 @@
 from flask import Blueprint
 from flask_cors import CORS
-from apscheduler.schedulers.background import BackgroundScheduler
-from app.utils import check_and_delete_unconfirmed_users
 
 routes = Blueprint('routes', __name__)
 CORS(routes)
-
-def start_scheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(func=check_and_delete_unconfirmed_users, trigger="interval", hours=24)
-    scheduler.start()
-
-start_scheduler()
--- app/routes/session.py
+++ app/routes/session.py
@@ -40,12 +40,12 @@ def create_token():
     if user and user.verify_password(password):
         if user.email_confirmed:
             access_token = create_access_token(identity=login)
-            response = {"email_confirmed": True, "access_token": access_token}
+            response = {"access_token": access_token}
             user.last_login = dt.utcnow()
             db.session.commit()
             return response
         else:
-            response = {"email_confirmed": False}
+            response = {"email_not_confirmed": True}
             return response
     else:
         return {"msg": "Wrong email or password"}, 401
